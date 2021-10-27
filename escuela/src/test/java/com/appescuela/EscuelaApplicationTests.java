bbaa55865707c965cf07138ca8c4e2bfdbaeee1f
AngelNahuat <nglnahuat@gmail.com>
2021-10-27 14:37:33 -0500

    prueba

---
+++ escuela/src/test/java/com/appescuela/EscuelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appescuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
