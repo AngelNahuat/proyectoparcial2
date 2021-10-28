723e02333fdb1af017a7b451ea75f0f4ac5d0b23
Fabian <fabian.kinil@cbtis72.edu.mx>
2021-10-28 18:46:09 -0500

    mensaje

--- escuela/src/main/java/com/appescuela/controller/HomeController.java
+++ escuela/src/main/java/com/appescuela/controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.appescuela.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-	public String motrarHome() {
+	public String motrarHome(Model model) {
 		
+		model.addAttribute("mensaje", "Bienvenidos a Pagina Escuela");
 		return "home";
 	}
 	
