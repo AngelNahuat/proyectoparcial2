fe18595dbb76013d18dd22f04815cb0f778d3f25
Fabian <fabian.kinil@cbtis72.edu.mx>
2021-10-29 08:09:53 -0500

    prueba

--- escuela/src/main/java/com/appescuela/controller/HomeController.java
+++ escuela/src/main/java/com/appescuela/controller/HomeController.java
@@ -20,11 +20,7 @@ public class HomeController {
 		
 		model.addAttribute("carreras", lista);
 		System.out.println("prueba");
-		System.out.println("prueba");
-		System.out.println("prueba");
-		System.out.println("prueba");
-		System.out.println("prueba");
-		System.out.println("XD");
+		
 
 		return "listado";
 	}
