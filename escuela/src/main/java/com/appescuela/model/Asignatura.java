c2cab740c6f0467e3b0a9e0f33b49c7c1c45acbd
Fabian <fabian.kinil@cbtis72.edu.mx>
2021-10-29 14:58:11 -0500

    Getters and setters

---
+++ escuela/src/main/java/com/appescuela/model/Asignatura.java
@@ -0,0 +1,33 @@
+package com.appescuela.model;
+
+public class Asignatura {
+
+	
+	private Integer CodAsignatura;
+	private String nombre;
+	private String Detalle;
+	
+	
+	
+	//Getters and setters
+	public Integer getCodAsignatura() {
+		return CodAsignatura;
+	}
+	public void setCodAsignatura(Integer codAsignatura) {
+		CodAsignatura = codAsignatura;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDetalle() {
+		return Detalle;
+	}
+	public void setDetalle(String detalle) {
+		Detalle = detalle;
+	}
+	
+	
+}
---
+++ escuela/src/main/java/com/appescuela/model/Profesor.java
@@ -0,0 +1,57 @@
+package com.appescuela.model;
+
+public class Profesor {
+
+	
+	private Integer IdProfesor;
+	private Integer NIF_P;
+	private String Nombre;
+	private String Especialidad;
+	private Integer Telefono;
+	private String Fotografia;
+	
+	
+	
+	public Integer getIdProfesor() {
+		return IdProfesor;
+	}
+	public void setIdProfesor(Integer idProfesor) {
+		IdProfesor = idProfesor;
+	}
+	public Integer getNIF_P() {
+		return NIF_P;
+	}
+	public void setNIF_P(Integer nIF_P) {
+		NIF_P = nIF_P;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getEspecialidad() {
+		return Especialidad;
+	}
+	public void setEspecialidad(String especialidad) {
+		Especialidad = especialidad;
+	}
+	public Integer getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(Integer telefono) {
+		Telefono = telefono;
+	}
+	public String getFotografia() {
+		return Fotografia;
+	}
+	public void setFotografia(String fotografia) {
+		Fotografia = fotografia;
+	}
+	
+	
+	
+	
+	
+	
+}
