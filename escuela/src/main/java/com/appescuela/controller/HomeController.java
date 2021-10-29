e61b3d2c0ad2cca889b941151aefcb886ded55b0
AngelNahuat <nglnahuat@gmail.com>
2021-10-28 21:41:58 -0500

    prueba

--- escuela/src/main/java/com/appescuela/controller/HomeController.java
+++ escuela/src/main/java/com/appescuela/controller/HomeController.java
@@ -22,6 +22,7 @@ public class HomeController {
 		System.out.println("prueba");
 		System.out.println("prueba");
 		System.out.println("prueba");
+		System.out.println("prueba");
 
 		return "listado";
 	}
