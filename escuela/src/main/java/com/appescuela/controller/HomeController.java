dcf7e4efb5d623f8988e0715dc67e7ef929b2db1
Fabian <fabian.kinil@cbtis72.edu.mx>
2021-10-28 19:22:59 -0500

    metodos

--- escuela/src/main/java/com/appescuela/controller/HomeController.java
+++ escuela/src/main/java/com/appescuela/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.appescuela.controller;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +11,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	@GetMapping("/listado")
+	public String motrarListado(Model model) {
+		
+		List<String> lista = new LinkedList<String>();
+		lista.add("Programacion");
+		lista.add("Contabilidad");
+		
+		model.addAttribute("carreras", lista);
+		return "listado";
+	}
+	
+	
 	@GetMapping("/")
 	public String motrarHome(Model model) {
 		
 		model.addAttribute("mensaje", "Bienvenidos a Pagina Escuela");
+		model.addAttribute("fecha", new Date() );
 		return "home";
 	}
 	
