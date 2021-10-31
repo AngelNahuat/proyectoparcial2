ab8d7505c013da1c105339a10ee7c2224d367feb
AngelNahuat <nglnahuat@gmail.com>
2021-10-31 17:31:09 -0500

    codigo eliminado

--- escuela/src/main/java/com/appescuela/controller/HomeController.java
+++ escuela/src/main/java/com/appescuela/controller/HomeController.java
@@ -97,8 +97,7 @@ public class HomeController {
 	@GetMapping("/")
 	public String motrarHome(Model model) {
 		     
-		model.addAttribute("mensaje", "Bienvenidos a Pagina/Escuela");
-		model.addAttribute("fecha", new Date() );
+	
 		return "home";
 	}
 	
